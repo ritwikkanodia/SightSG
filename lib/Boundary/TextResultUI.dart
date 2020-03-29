0b125a5f7081a9fac84aab779ec9945cb6128aa3
ritwikkanodia <44768122+ritwikkanodia@users.noreply.github.com>
2020-03-29 18:54:43 +0800

    Update TextResultUI.dart

--- lib/Boundary/TextResultUI.dart
+++ lib/Boundary/TextResultUI.dart
@@ -4,60 +4,124 @@ import 'dart:typed_data';
 import 'package:assignment_app/Control/ArchiveController.dart';
 import 'package:flutter/material.dart';
 
-class TextResultUI extends StatefulWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
+import 'gradient_app_bar.dart';
+import 'size_config.dart';
+
+void main() => runApp(MaterialApp(
+  home:  NinjaCard(),  //Determines what is gonna show on the home screen of our app
+));
+
+//Lets create a stateless widget which will return a widget tree which will show on our home page
+
+class NinjaCard extends StatefulWidget {
   @override
-  _TextResultUIState createState() => _TextResultUIState();
+  _NinjaCardState createState() => _NinjaCardState();
 }
 
-class _TextResultUIState extends State<TextResultUI> {
-  List<Uint8List> _image;
-  Future<File> getImage() async{
-    List<Uint8List> image = await ArchiveController.downloadPicture();
-    this.setState(() {
-      this._image = image;
-    });
-  }
+class _NinjaCardState extends State<NinjaCard> {
 
+  double fontSize = 10.0;
+  List <String> test = ["'Please end this meeting'", "'Before my Brain melts'"]; //The list of string being displayed
 
+  String imageName = "'assets/Meme 1 - Please end this meeting.jpg'";
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    final double h = SizeConfig.blockSizeVertical;
+    final double w = SizeConfig.blockSizeHorizontal;
+
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            //child: _image == null ? Image.asset('assets/placeholder.jpg') : Image.memory(_image),
+        backgroundColor: Colors.white,
+        appBar: GradientAppBar(
+          centerTitle: true,
+          backgroundColorStart: Colors.blue,
+          backgroundColorEnd: Colors.green,
+          title: Text(
+            'Text Results',
+            style: (TextStyle(fontSize: (h * 4))),
           ),
-          Padding(
-            padding:EdgeInsets.symmetric(horizontal: 5, vertical: (5)),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
-                ),
-              ),
-              child: ListTile(
-                leading: Icon(
-                  Icons.text_fields,
-                  size: 40,
-                  color: Colors.white,
-                ),
-                title: Text(
-                  'Upload',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+        ),
+
+
+        body: Padding(
+            padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 30.0),
+            child: ListView(
+              //crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Image.asset(
+          'assets/Meme 1 - Please end this meeting.jpg',
+                        height: 200,
+                        width: 200,
+        ),
+
+                  Divider(
+                    height: 60.0, //Height of space above and below divider
+                    color: Colors.blue,
                   ),
-                ),
-                onTap: () {
-                  getImage();
-                },
-              ),
-            ),
+
+                  for ( var i in test)
+                   Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                                '$i',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: fontSize,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 2.0, //2 pixels between letters
+                                )
+
+                            ),
+
+
+                            SizedBox(height: 40.0),
+                          ]
+                      ),
+
+
+    Row(
+    children: <Widget>[
+     SizedBox(width: 293.0),
+      SizedBox(height: 150.0),
+                  Column(
+                         children: <Widget>[
+
+                           FloatingActionButton(
+                              onPressed: () {
+                              setState(() {
+                                fontSize += 10;
+                            });
+                              },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.blue,
+        ),
+
+                           SizedBox(height: 10.0),
+
+          FloatingActionButton(
+            onPressed: () {
+            setState(() {
+            fontSize -= 10;
+            });
+           },
+          child: Icon(Icons.remove),
+          backgroundColor: Colors.blue,
           ),
-        ],
-      )
+                         ]
+
+    ),
+
+                         ]
+    )
+    ]
+    )
+                  )
+
     );
+
   }
 }
